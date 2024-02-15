55408ddd2a21bb912732763829b8768a876153eb
PH4NTOM <Simonmadsen99@gmail.com>
2024-02-15 21:31:27 +0100

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './css/stylesheet.css'
+import CartItems from "./pageelements/CartItems.tsx";
 
 function App() {
   return (
     <>
       <h1>VitaminBixen</h1>
+      <CartItems/>
     </>
   )
 }
---
+++ src/pageelements/CartItems.tsx
@@ -0,0 +1,11 @@
+import '../css/cartitems.css'
+
+function CartItems() {
+    return (
+        <>
+            <p>Test :)</p>
+        </>
+    )
+}
+
+export default CartItems
