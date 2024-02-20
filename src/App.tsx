eebd1027254216941e7900e1848d7e99d998ce95
IntelliLakshmi <mettea652@gmail.com>
2024-02-20 23:56:53 +0100

    a bit messy header added to the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>VitaminBixen</h1>
-    </>
-  )
+    <header className="header">
+      <div className="logo">
+        <span>VitaminBiksen</span> {/* put ind logo */}
+      </div>
+      <nav className="nav">
+        <a href="#multi">Multi Vitaminer</a>
+        <a href="#c">C Vitaminer</a>
+        <a href="#d">D Vitaminer</a>
+        <a href="#b">B Vitaminer</a>
+        <a href="#more">Mere</a>
+      </nav>
+      <div className="search-bar">
+        <input type="text" placeholder="Tænk vitaminer, køb vitaminer" />
+        <button type="submit">🔍</button>
+      </div>
+      <div className="header-right">
+        <a href="#business">Erhverv</a>
+        <a href="#service">Kundeservice</a>
+        <div className="cart">
+          <span>Kurv</span> {/* Replace with cart icon */}
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default App
+export default App;
