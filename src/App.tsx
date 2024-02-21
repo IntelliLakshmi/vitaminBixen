fc19055722b5cabd6e7f41f0c5deb79a1daeabb1
IntelliLakshmi <mettea652@gmail.com>
2024-02-21 18:48:41 +0100

    messy header with outline from prototype

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,32 @@ import './App.css';
 function App() {
   return (
     <header className="header">
-      <div className="logo">
-        <span>VitaminBiksen</span> {/* put ind logo */}
+      <div className='topbarContainer'>
+        <div className="logo">
+          <span>VitaminBiksen</span> {/* put the logo in here */}
+        </div>
+        <div className="search-bar">
+          <input type="text" placeholder="Tænk vitaminer, køb vitaminer" />
+            <button type="submit">🔍</button>
+        </div>
+        <div className="header-right">
+        <a href="#business">Erhverv</a>
+        <a href="#service">Kundeservice</a>
+          <div className="cart">
+            <span>Kurv</span> {/* ersatte med kruv icon */}
+          </div>
+        </div>
       </div>
-      <nav className="nav">
+      <div className='lowbarContainer'>
+        <nav className="nav">
         <a href="#multi">Multi Vitaminer</a>
         <a href="#c">C Vitaminer</a>
         <a href="#d">D Vitaminer</a>
         <a href="#b">B Vitaminer</a>
         <a href="#more">Mere</a>
-      </nav>
-      <div className="search-bar">
-        <input type="text" placeholder="Tænk vitaminer, køb vitaminer" />
-        <button type="submit">🔍</button>
+        </nav>
       </div>
-      <div className="header-right">
-        <a href="#business">Erhverv</a>
-        <a href="#service">Kundeservice</a>
-        <div className="cart">
-          <span>Kurv</span> {/* Replace with cart icon */}
-        </div>
-      </div>
+      
     </header>
   );
 }
