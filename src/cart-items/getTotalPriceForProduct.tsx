0a003e4204110666220aad740d7186aaeb56e5a1
IntelliLakshmi <mettea652@gmail.com>
2024-03-10 13:28:29 +0100

    nugdemessage and rebate

--- src/cart-items/getTotalPriceForProduct.tsx
+++ src/cart-items/getTotalPriceForProduct.tsx
@@ -3,8 +3,17 @@ import { Item } from "./model/Item.tsx";
 export function getTotalPriceForProduct(
   basket: Item[],
   productID: string
-): number {
+): { totalPrice: number, rebate: number } {
   const product = basket.find((item) => item.id === productID);
   const giftWrapPrice = product?.giftWrap ? 10 : 0;
-  return product ? product.price * product.amount + giftWrapPrice : 0;
+  let rebate = 0;
+
+  // Tjek om mængden er 5 eller mere og beregn rabatten.
+  if (product && product.amount >= 5) {
+    rebate = product.price * product.amount * 0.10; // 10% rabat
+  }
+
+  const totalPrice = product ? (product.price * product.amount + giftWrapPrice) - rebate : 0;
+
+  return { totalPrice, rebate };
 }
