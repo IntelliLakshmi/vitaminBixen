2ad0208c3fb92e48b23a5d98fee6f6a5996409b5
PH4MTOM <Simonmadsen99@gmail.com>
2024-03-11 16:13:49 +0100

    Added test!

--- tests/components/Contactform.test.tsx
+++ tests/components/Contactform.test.tsx
@@ -1,6 +1,7 @@
 import {render, fireEvent, screen} from "@testing-library/react";
 import {describe, it, expect} from "vitest";
 import { waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../../src/App.tsx";
 
 describe('Testing Contactform', async () => {
@@ -12,11 +13,11 @@ describe('Testing Contactform', async () => {
         // Find input field for Fornavn, simulate writing in the field and then click away from the field.
         const inputField = screen.getByLabelText('Fornavn*');
         fireEvent.change(inputField, { target: { value: '' }});
-        fireEvent.blur(inputField)
+        fireEvent.blur(inputField);
 
         // Search for the error message on the screen
         const errorMessage = screen.getByText('Udfyld venligst dette felt');
-        expect(errorMessage).toBeInTheDocument()
+        expect(errorMessage).toBeInTheDocument();
     });
 
     it('Forname should not display error message "Udfyld venligst dette felt" when empty', async () => {
@@ -25,44 +26,184 @@ describe('Testing Contactform', async () => {
         // Find input field for Fornavn, simulate writing in the field and then click away from the field.
         const inputField = screen.getByLabelText('Fornavn*');
         fireEvent.change(inputField, { target: { value: 'John' }});
-        fireEvent.blur(inputField)
+        fireEvent.blur(inputField);
 
         // Search for the error message on the screen
         await waitFor(() => {
             const errorMessage = screen.queryByText('Udfyld venligst dette felt');
-            expect(errorMessage).not.toBeInTheDocument()
+            expect(errorMessage).not.toBeInTheDocument();
         });
     });
 
-    /*
-    // Testing Valid Emails Logic
-    it('field Email should accept the following emails', () => {
-        const setContactAndDeliveryFormData = vi.fn();
-        const { getByLabelText, queryByText } = render(
-            <ContactForm
-                formSubmitted={false}
-                formData={initialFormData}
-                setContactAndDeliveryFormData={setContactAndDeliveryFormData}
-            />
-        );
+    //Efternavn Field Testing
+    it('Efternavn should display error message "Udfyld venligst dette felt" when empty', async () => {
+        render(<App />);
 
-        // Find input field for Fornavn, simulate writing in the field and then click away from the field.
-        const inputField = getByLabelText('Email*');
+        const inputField = screen.getByLabelText('Efternavn*');
         fireEvent.change(inputField, { target: { value: '' }});
-        fireEvent.blur(inputField)
-    });
-     */
+        fireEvent.blur(inputField);
 
-    /*
-    it('field Email shouldnt accept the following emails and display "ugyldig email"', () => {
-        // Testing Invalid Emails Logic
-    });
-
-    it('field Telefonnummer should display "Telefonnummer skal være 8 tegn langt." when number is too short', () => {
-        // Testing Short Number Logic
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('Udfyld venligst dette felt');
+            expect(errorMessage).toBeInTheDocument();
+        });
     });
 
- */
+    it('Efternavn should not display error message "Udfyld venligst dette felt" when empty', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Efternavn*');
+        fireEvent.change(inputField, { target: { value: 'Madsen' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('Udfyld venligst dette felt');
+            expect(errorMessage).not.toBeInTheDocument();
+        });
+    });
+
+    //  Email Field Testing
+    it('Email should display error message "Udfyld venligst dette felt" when empty', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Email*');
+        fireEvent.change(inputField, { target: { value: '' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('Udfyld venligst dette felt');
+            expect(errorMessage).toBeInTheDocument();
+        });
+    });
+
+    // Testing Invalid Emails Logic 1
+    it('field Email shouldnt accept the following emails and display "ugyldig email"', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Email*');
+        fireEvent.change(inputField, { target: { value: '#@%^%#$@#$@#.com' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('ugyldig email');
+            expect(errorMessage).toBeInTheDocument();
+        });
+    });
+
+    // Testing Invalid Emails Logic 1
+    it('field Email shouldnt accept the following emails and display "ugyldig email"2', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Email*');
+        fireEvent.change(inputField, { target: { value: 'email@example.com (Joe Smith)' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('ugyldig email');
+            expect(errorMessage).toBeInTheDocument();
+        });
+    });
+
+    // Testing Valid Email Logic
+    it('field Email should accept the following emails and should not display "ugyldig email"', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Email*');
+        fireEvent.change(inputField, { target: { value: 'mom-mm@vip.cybercity.dk' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('ugyldig email');
+            expect(errorMessage).not.toBeInTheDocument();
+        });
+    });
+
+    // Testing Valid Email Logic
+    it('field Email should accept the following emails and should not display "ugyldig email"2', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Email*');
+        fireEvent.change(inputField, { target: { value: 'simonmadsen99@gmail.com' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('ugyldig email');
+            expect(errorMessage).not.toBeInTheDocument();
+        });
+    });
+
+    // Phone Number Field Testing
+    it('field Telefonnummer should display "Udfyld venligst dette felt" when number is too short', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Telefonnummer*');
+        fireEvent.change(inputField, { target: { value: '' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('Udfyld venligst dette felt');
+            expect(errorMessage).toBeInTheDocument();
+        });
+    });
+
+    // Testing number short number error logic
+    it('field Telefonnummer should display "Telefonnummer skal være 8 tegn langt." when number is too short', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Telefonnummer*');
+        fireEvent.change(inputField, { target: { value: '9999999' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('Telefonnummer skal være 8 tegn langt.');
+            expect(errorMessage).toBeInTheDocument();
+        });
+    });
+
+    // Testing number correct logic
+    it('field Telefonnummer should not display any errors, when typed in correctly', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Telefonnummer*');
+        fireEvent.change(inputField, { target: { value: '99999999' }});
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('Telefonnummer skal være 8 tegn langt.');
+            expect(errorMessage).not.toBeInTheDocument();
+        });
+    });
+
+    // User cannot type in chars test.
+    it('field Telefonnummer should not allow users to type in chars', async () => {
+        render(<App />);
+
+        const inputField = screen.getByLabelText('Telefonnummer*');
+        const inputField2 = screen.getByLabelText('Fornavn*');
+
+        fireEvent.change(inputField2, { target: { value: 'Simon' }});
+
+        await userEvent.type(inputField, 'testtest');
+        fireEvent.blur(inputField);
+
+        // Search for the error message on the screen
+        await waitFor(() => {
+            const errorMessage = screen.queryByText('Udfyld venligst dette felt');
+            expect(errorMessage).toBeInTheDocument();
+        });
+    });
+
+
 
     //More test to add.
 
